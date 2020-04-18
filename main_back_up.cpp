488e9988f63a4a0b5f858077310327e62c492343
ustc-keyanjie <770709456@qq.com>
2020-04-18 13:59:53 +0800

    update

--- main_back_up.cpp
+++ main_back_up.cpp
@@ -1,10 +1,7 @@
 // submit:
 // 1. delete #define TEST.
-// 2. open //#define MMAP
-// 3. change NUM_LEN7_RESULT to 3000000
 
-#define TEST
-//#define MMAP
+//#define TEST
 #include <bits/stdc++.h>
 #include <fcntl.h>
 #include <stdio.h>
@@ -13,21 +10,19 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-#ifdef MMAP
 #include <linux/fb.h>
 #include <sys/ioctl.h>
 #include <sys/mman.h>
-#endif
 
-#define MAX_NUM_EDGES 280000
-#define MAX_NUM_IDS 262144
-#define MAX_OUT_DEGREE 51
+#define MAX_NUM_EDGES 280000 // 可接受的最大边数
+#define MAX_NUM_IDS 65536 * 4   // 可接受的最大结点数
+#define MAX_OUT_DEGREE 51    // 可接受的最大出度
 
 #define NUM_LEN3_RESULT 500000
 #define NUM_LEN4_RESULT 500000
 #define NUM_LEN5_RESULT 1000000
 #define NUM_LEN6_RESULT 2000000
-#define NUM_LEN7_RESULT 3200000
+#define NUM_LEN7_RESULT 3200000 // 提交时改成3000000
 
 #define MAX_INT 2147483647
 
@@ -35,20 +30,30 @@ using namespace std;
 
 int id_num = 0, edge_num = 0, res_count = 0;
 
+// store edge info (u-v)
 int u_ids[MAX_NUM_EDGES];
 int v_ids[MAX_NUM_EDGES];
-int ids[MAX_NUM_EDGES * 2];
 
-int g_succ[MAX_NUM_IDS][MAX_OUT_DEGREE];
-int out_degree[MAX_NUM_IDS];
+int g_succ[MAX_NUM_IDS][MAX_OUT_DEGREE]; // 图的邻接表
+int out_degree[MAX_NUM_IDS];             // 每个结点的出度，也可用作邻接表的范围限定
 
+// DFS部分
 int path[7];
 bool visited[MAX_NUM_IDS];
 
+// 反向二级跳表部分
 vector<unordered_map<int, vector<int>>> two_uj;
 bool reachable[MAX_NUM_IDS];
 vector<int> currentJs;
 
+// results
+// e.g.
+// for path length 6 [9, 5, 6, 2, 0, 3]
+// for (int i = 0; i < 6; ++i)
+// {
+//     results[6 - 3][num_res6 * 6 + i] = path[i];
+// }
+// num_res6++;
 int res3[NUM_LEN3_RESULT * 3];
 int res4[NUM_LEN4_RESULT * 4];
 int res5[NUM_LEN5_RESULT * 5];
@@ -57,29 +62,7 @@ int res7[NUM_LEN7_RESULT * 7];
 int num_res[5] = {0};
 int *results[] = {res3, res4, res5, res6, res7};
 
-void input_fstream(const string &testFile)
-{
-#ifdef TEST
-    clock_t input_time = clock();
-#endif
-
-    FILE *file = fopen(testFile.c_str(), "r");
-    int money;
-    while (fscanf(file, "%d,%d,%d\n", u_ids + edge_num, v_ids + edge_num, &money) != EOF)
-    {
-        ids[(edge_num << 1)] = u_ids[edge_num];
-        ids[(edge_num << 1) + 1] = v_ids[edge_num];
-        edge_num++;
-    }
-    fclose(file);
-#ifdef TEST
-    cout << "fscanf input time " << (double)(clock() - input_time) / CLOCKS_PER_SEC << "s" << endl;
-#endif
-}
-
-#ifdef MMAP
-
-void input_mmap(string &testFile)
+void input_data(string &testFile, set<int> &ids_set)
 {
 #ifdef TEST
     clock_t input_time = clock();
@@ -104,63 +87,23 @@ void input_mmap(string &testFile)
     char *token = strtok(buf, ",\n");
     while (token != NULL)
     {
-        ids[(edge_num << 1)] = atoi(token);
-        u_ids[edge_num] = ids[(edge_num << 1)];
+        u_ids[edge_num] = atoi(token);
+        ids_set.insert(u_ids[edge_num]);
         token = strtok(NULL, ",\n");
-        ids[(edge_num << 1) + 1] = atoi(token);
-        v_ids[edge_num++] = ids[(edge_num << 1) + 1];
+        v_ids[edge_num] = atoi(token);
+        ids_set.insert(v_ids[edge_num++]);
         token = strtok(NULL, ",\n");
         token = strtok(NULL, ",\n");
     }
     close(fd);
     munmap(p_map, length);
 #ifdef TEST
-    cout << "mmap input time " << (double)(clock() - input_time) / CLOCKS_PER_SEC << "s" << endl;
+    cout << "Input time " << (double)(clock() - input_time) / CLOCKS_PER_SEC << "s" << endl;
 #endif
 }
 
-#endif
-
-void save_fwrite(const string &resultFile, vector<string> &idsComma, vector<string> &idsLF)
-{
-
-#ifdef TEST
-    clock_t write_time = clock();
-    printf("Total Loops %d\n", res_count);
-#endif
-
-    FILE *fp = fopen(resultFile.c_str(), "w");
-    char *str_res = (char *)malloc(512 * 1024 * 1024); // 512M space
-    int str_len = sprintf(str_res, "%d\n", res_count);
-
-    for (int res_len = 3; res_len <= 7; ++res_len)
-    {
-        for (int line = 0; line < num_res[res_len - 3]; ++line)
-        {
-            for (int id = 0; id < res_len - 1; ++id)
-            {
-                const char *res = idsComma[results[res_len - 3][line * res_len + id]].c_str();
-                memcpy(str_res + str_len, res, strlen(res));
-                str_len += strlen(res);
-            }
-            const char *res = idsLF[results[res_len - 3][line * res_len + res_len - 1]].c_str();
-            memcpy(str_res + str_len, res, strlen(res));
-            str_len += strlen(res);
-        }
-    }
-
-    str_res[str_len] = '\0';
-    fwrite(str_res, sizeof(char), str_len, fp);
-    fclose(fp);
-
-#ifdef TEST
-    cout << "fwrite output time " << (double)(clock() - write_time) / CLOCKS_PER_SEC << "s" << endl;
-#endif
-}
-
-#ifdef MMAP
 // memcpy version
-void save_mmap(const string &resultFile, vector<string> &idsComma, vector<string> &idsLF)
+void save_results(const string &resultFile, vector<string> &idsComma, vector<string> &idsLF)
 {
 #ifdef TEST
     clock_t write_time = clock();
@@ -177,10 +120,13 @@ void save_mmap(const string &resultFile, vector<string> &idsComma, vector<string
 
     int str_len = idx;
 
+    // 结果长度
     for (int res_len = 3; res_len <= 7; ++res_len)
     {
+        // 每行结果
         for (int line = 0; line < num_res[res_len - 3]; ++line)
         {
+            // 环中每个id
             for (int id = 0; id < res_len - 1; ++id)
             {
                 const char *res = idsComma[results[res_len - 3][line * res_len + id]].c_str();
@@ -205,13 +151,13 @@ void save_mmap(const string &resultFile, vector<string> &idsComma, vector<string
     close(fd);
 
 #ifdef TEST
-    cout << "mmap output time " << (double)(clock() - write_time) / CLOCKS_PER_SEC << "s" << endl;
+    cout << "Output time " << (double)(clock() - write_time) / CLOCKS_PER_SEC << "s" << endl;
 #endif
 }
-#endif
 
 void dfs(int start_id, int cur_id, int depth)
 {
+    // 只搜索比start大的点，由于哨兵机制，不需要判断begin会越界，因为最后一个数是INT_MAX，start_id一定小于它
     int begin = 0;
     while (start_id >= g_succ[cur_id][begin])
         ++begin;
@@ -222,13 +168,17 @@ void dfs(int start_id, int cur_id, int depth)
     visited[cur_id] = true;
     path[depth] = cur_id;
 
+    // 搜索所有大于start的后继
     for (; begin < out_degree[cur_id]; ++begin)
     {
         int next_id = g_succ[cur_id][begin];
+        // 后继被访问过
         if (!visited[next_id])
         {
+            // 后继可直接到达起点，就找到一个环
             if (reachable[next_id])
             {
+                // find circuits
                 for (int &k : two_uj[start_id][next_id])
                 {
                     if (!visited[k])
@@ -236,6 +186,8 @@ void dfs(int start_id, int cur_id, int depth)
                         path[depth + 1] = next_id;
                         path[depth + 2] = k;
 
+                        // int length = depth + 3;
+                        // memcpy(results[depth] + num_res[depth] * length, path, sizeof(int) * (depth + 3));
                         memcpy(results[depth] + num_res[depth] * (depth + 3), path, 4 * depth + 12);
                         ++num_res[depth];
                         ++res_count;
@@ -249,42 +201,6 @@ void dfs(int start_id, int cur_id, int depth)
     }
 
     visited[cur_id] = false;
-}
-
-int binary_search(int target)
-{
-    int left = 0;
-    int right = id_num - 1;
-    while (left <= right)
-    {
-        int mid = (right + left) >> 1;
-        if (ids[mid] == target)
-        {
-            return mid;
-        }
-        else if (target > ids[mid])
-        {
-            left = mid + 1;
-        }
-        else if (target < ids[mid])
-        {
-            right = mid - 1;
-        }
-    }
-    return -1;
-}
-
-void duplicate_removal()
-{
-    int slow = 0, fast = 1;
-    int double_edge_num = edge_num << 1;
-    while (fast < double_edge_num)
-    {
-        if (ids[fast] != ids[slow])
-            ids[++slow] = ids[fast];
-        fast++;
-    }
-    id_num = slow + 1;
 }
 
 int main()
@@ -308,29 +224,39 @@ int main()
     clock_t start_time = clock();
 #endif
 
+    set<int> ids_set;
+    vector<int> ids;                 // regular-id(0, 1, 2, ..., n-1) to really-id
+    unordered_map<int, int> id_hash; // really-id to regular-id(0, 1, 2, ..., n-1)
+
     vector<string> idsComma(MAX_NUM_IDS); // id + ','
     vector<string> idsLF(MAX_NUM_IDS);    // id + '\n'
 
-    // input_mmap
-#ifdef MMAP
-    // input_mmap(testFile);
-#else
-    input_fstream(testFile);
-#endif
+    // input_data
+    input_data(testFile, ids_set);
 
-    sort(ids, ids + (edge_num << 1));
-    duplicate_removal();
+    ids = vector<int>(ids_set.begin(), ids_set.end());
+    id_num = ids.size();
+
+    two_uj.resize(id_num);
+
+    for (int i = 0; i < id_num; ++i)
+    {
+        id_hash[ids[i]] = i;
+    }
 
     for (int i = 0; i < edge_num; ++i)
     {
-        int u = binary_search(u_ids[i]);
-        g_succ[u][out_degree[u]++] = binary_search(v_ids[i]);
+        int u = id_hash[u_ids[i]], v = id_hash[v_ids[i]];
+        g_succ[u][out_degree[u]++] = v;
     }
 
     for (int i = 0; i < id_num; ++i)
     {
         sort(g_succ[i], g_succ[i] + out_degree[i]);
+        // 哨兵机制，dfs刚开始确定begin时，只需要判断start和其后继的值即可，不需要判断越界
+        // 存在产生bug的可能性：刚好有结点出度为50，那么这里赋值时会造成数组越界
         g_succ[i][out_degree[i]] = MAX_INT;
+        // 这两个数组真的有必要吗？可以输出时动态生成 to_string(ids[i]) + "," 这样的数据吗？
         idsComma[i] = to_string(ids[i]) + ",";
         idsLF[i] = to_string(ids[i]) + "\n";
     }
@@ -339,7 +265,7 @@ int main()
     clock_t two_uj_time = clock();
 #endif
 
-    two_uj.resize(id_num);
+    // 构建反向二级跳表
     for (int u = 0; u < id_num; u++)
     {
         int *succ1 = g_succ[u];
@@ -362,6 +288,7 @@ int main()
     cout << "Construct Jump Table " << (double)(clock() - two_uj_time) / CLOCKS_PER_SEC << "s" << endl;
     clock_t search_time = clock();
 #endif
+    // DFS搜索
     for (int start_id = 0; start_id < id_num; ++start_id)
     {
 #ifdef TEST
@@ -391,11 +318,7 @@ int main()
     cout << "DFS " << (double)(clock() - search_time) / CLOCKS_PER_SEC << "s" << endl;
 #endif
 
-#ifdef MMAP
-    save_mmap(resultFile, idsComma, idsLF);
-#else
-    save_fwrite(resultFile, idsComma, idsLF);
-#endif
+    save_results(resultFile, idsComma, idsLF);
 
 #ifdef TEST
     cout << "Total time " << (double)(clock() - start_time) / CLOCKS_PER_SEC << "s" << endl;
