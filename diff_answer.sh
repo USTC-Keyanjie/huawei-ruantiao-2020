a756e7430eca60862afe37f73ad148c0a56d2492
ustc-keyanjie <770709456@qq.com>
2020-05-16 18:32:14 +0800

    去除答案比较

--- diff_answer.sh
+++ diff_answer.sh
@@ -1,56 +1,43 @@
 g++ -O3 semi_final.cpp -o semi_final -lpthread -fpic
 rm -rf test_data_fs/std/result.txt
 ./semi_final std
-diff test_data_fs/std/answer.txt test_data_fs/std/result.txt
 
 g++ -O3 semi_final.cpp -o semi_final -lpthread -fpic
 rm -rf test_data_fs/41/result.txt
 ./semi_final 41
-diff test_data_fs/41/answer.txt test_data_fs/41/result.txt
 
 g++ -O3 semi_final.cpp -o semi_final -lpthread -fpic
 rm -rf test_data_fs/294/result.txt
 ./semi_final 294
-diff test_data_fs/294/answer.txt test_data_fs/294/result.txt
 
 g++ -O3 semi_final.cpp -o semi_final -lpthread -fpic
 rm -rf test_data_fs/34195/result.txt
 ./semi_final 34195
-diff test_data_fs/34195/answer.txt test_data_fs/34195/result.txt
 
 g++ -O3 semi_final.cpp -o semi_final -lpthread -fpic
 rm -rf test_data_fs/51532/result.txt
 ./semi_final 51532
-diff test_data_fs/51532/answer.txt test_data_fs/51532/result.txt
 
 g++ -O3 semi_final.cpp -o semi_final -lpthread -fpic
 rm -rf test_data_fs/639096/result.txt
 ./semi_final 639096
-diff test_data_fs/639096/answer.txt test_data_fs/639096/result.txt
 
 g++ -O3 semi_final.cpp -o semi_final -lpthread -fpic
 rm -rf test_data_fs/697518/result.txt
 ./semi_final 697518
-diff test_data_fs/697518/answer.txt test_data_fs/697518/result.txt
 
 g++ -O3 semi_final.cpp -o semi_final -lpthread -fpic
 rm -rf test_data_fs/2408026/result.txt
 ./semi_final 2408026
-diff test_data_fs/2408026/answer.txt test_data_fs/2408026/result.txt
 
 g++ -O3 semi_final.cpp -o semi_final -lpthread -fpic
 rm -rf test_data_fs/2541581/result.txt
 ./semi_final 2541581
-diff test_data_fs/2541581/answer.txt test_data_fs/2541581/result.txt
 
 rm -rf test_data_fs/18875018/result.txt
-sleep 5
 g++ -O3 semi_final.cpp -o semi_final -lpthread -fpic
 ./semi_final 18875018
-diff test_data_fs/18875018/answer.txt test_data_fs/18875018/result.txt
 
 rm -rf test_data_fs/19630345/result.txt
-sleep 5
 g++ -O3 semi_final.cpp -o semi_final -lpthread -fpic
 ./semi_final 19630345
-diff test_data_fs/19630345/answer.txt test_data_fs/19630345/result.txt
