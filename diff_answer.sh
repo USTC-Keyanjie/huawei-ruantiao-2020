82031b08fdfb421b8da3d853387cd869ad8e09a8
ustc-keyanjie <770709456@qq.com>
2020-05-24 15:24:55 +0800

    修改脚本

--- diff_answer.sh
+++ diff_answer.sh
@@ -1,43 +1,14 @@
-g++ -O3 semi_final.cpp -o semi_final -lpthread -fpic
-rm -rf test_data_fs/std/result.txt
-./semi_final std
+g++ -O3 final.cpp -o final -lpthread -fpic
+rm -rf test_data_js/0/result.txt
+./final 0
+diff test_data_js/0/result.txt test_data_js/0/answer.txt
 
-g++ -O3 semi_final.cpp -o semi_final -lpthread -fpic
-rm -rf test_data_fs/41/result.txt
-./semi_final 41
+g++ -O3 final.cpp -o final -lpthread -fpic
+rm -rf test_data_js/1/result.txt
+./final 1
+diff test_data_js/1/result.txt test_data_js/1/answer.txt
 
-g++ -O3 semi_final.cpp -o semi_final -lpthread -fpic
-rm -rf test_data_fs/294/result.txt
-./semi_final 294
-
-g++ -O3 semi_final.cpp -o semi_final -lpthread -fpic
-rm -rf test_data_fs/34195/result.txt
-./semi_final 34195
-
-g++ -O3 semi_final.cpp -o semi_final -lpthread -fpic
-rm -rf test_data_fs/51532/result.txt
-./semi_final 51532
-
-g++ -O3 semi_final.cpp -o semi_final -lpthread -fpic
-rm -rf test_data_fs/639096/result.txt
-./semi_final 639096
-
-g++ -O3 semi_final.cpp -o semi_final -lpthread -fpic
-rm -rf test_data_fs/697518/result.txt
-./semi_final 697518
-
-g++ -O3 semi_final.cpp -o semi_final -lpthread -fpic
-rm -rf test_data_fs/2408026/result.txt
-./semi_final 2408026
-
-g++ -O3 semi_final.cpp -o semi_final -lpthread -fpic
-rm -rf test_data_fs/2541581/result.txt
-./semi_final 2541581
-
-rm -rf test_data_fs/18875018/result.txt
-g++ -O3 semi_final.cpp -o semi_final -lpthread -fpic
-./semi_final 18875018
-
-rm -rf test_data_fs/19630345/result.txt
-g++ -O3 semi_final.cpp -o semi_final -lpthread -fpic
-./semi_final 19630345
+g++ -O3 final.cpp -o final -lpthread -fpic
+rm -rf test_data_js/2/result.txt
+./final 2
+diff test_data_js/2/result.txt test_data_js/2/answer.txt
