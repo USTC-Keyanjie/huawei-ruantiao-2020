9378ab13e07ee9801b95795a596d485e074ce946
ustc-keyanjie <770709456@qq.com>
2020-05-25 07:25:47 +0800

    兼容数据集3

--- diff_answer.sh
+++ diff_answer.sh
@@ -1,14 +1,19 @@
+git pull
+
 g++ -O3 final.cpp -o final -lpthread -fpic
+
 rm -rf test_data_js/0/result.txt
 ./final 0
 diff test_data_js/0/result.txt test_data_js/0/answer.txt
 
-g++ -O3 final.cpp -o final -lpthread -fpic
 rm -rf test_data_js/1/result.txt
 ./final 1
 diff test_data_js/1/result.txt test_data_js/1/answer.txt
 
-g++ -O3 final.cpp -o final -lpthread -fpic
 rm -rf test_data_js/2/result.txt
 ./final 2
 diff test_data_js/2/result.txt test_data_js/2/answer.txt
+
+rm -rf test_data_js/3/result.txt
+./final 3
+diff test_data_js/3/result.txt test_data_js/3/answer.txt
