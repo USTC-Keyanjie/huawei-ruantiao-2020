9309b151faf59ba9e8607dca30749c75cc0f6640
ustc-keyanjie <770709456@qq.com>
2020-05-30 08:29:08 +0800

    更新

--- diff_answer.sh
+++ diff_answer.sh
@@ -3,102 +3,13 @@ git pull
 rm -rf ./final
 g++ final.cpp -o final -O3 -g -lpthread -fpic
 
-rm -rf test_data_js/0/result.txt
-./final 0
-diff test_data_js/0/result.txt test_data_js/0/answer.txt
-
-rm -rf test_data_js/1/result.txt
-./final 1
-diff test_data_js/1/result.txt test_data_js/1/answer.txt
-
-rm -rf test_data_js/2/result.txt
-./final 2
-diff test_data_js/2/result.txt test_data_js/2/answer.txt
-
-rm -rf test_data_js/3/result.txt
-./final 3
-diff test_data_js/3/result.txt test_data_js/3/answer.txt
-
-rm -rf test_data_js/4/result.txt
-./final 4
-diff test_data_js/4/result.txt test_data_js/4/answer.txt
-
-rm -rf test_data_js/5/result.txt
-./final 5
-diff test_data_js/5/result.txt test_data_js/5/answer.txt
-
-rm -rf test_data_js/6/result.txt
-./final 6
-diff test_data_js/6/result.txt test_data_js/6/answer.txt
-
-rm -rf test_data_js/7/result.txt
-./final 7
-diff test_data_js/7/result.txt test_data_js/7/answer.txt
-
-rm -rf test_data_js/8/result.txt
-./final 8
-diff test_data_js/8/result.txt test_data_js/8/answer.txt
-
-rm -rf test_data_js/9/result.txt
-./final 9
-diff test_data_js/9/result.txt test_data_js/9/answer.txt
-
-rm -rf test_data_js/10/result.txt
-./final 10
-diff test_data_js/10/result.txt test_data_js/10/answer.txt
-
-rm -rf test_data_js/11/result.txt
-./final 11
-diff test_data_js/11/result.txt test_data_js/11/answer.txt
-
-rm -rf test_data_js/12/result.txt
-./final 12
-diff test_data_js/12/result.txt test_data_js/12/answer.txt
-
-rm -rf test_data_js/13/result.txt
-./final 13
-diff test_data_js/13/result.txt test_data_js/13/answer.txt
-
-rm -rf test_data_js/14/result.txt
-./final 14
-diff test_data_js/14/result.txt test_data_js/14/answer.txt
-
-rm -rf test_data_js/15/result.txt
-./final 15
-diff test_data_js/15/result.txt test_data_js/15/answer.txt
-
-rm -rf test_data_js/16/result.txt
-./final 16
-diff test_data_js/16/result.txt test_data_js/16/answer.txt
-
-rm -rf test_data_js/17/result.txt
-./final 17
-diff test_data_js/17/result.txt test_data_js/17/answer.txt
-
-rm -rf test_data_js/18/result.txt
-./final 18
-diff test_data_js/18/result.txt test_data_js/18/answer.txt
-
-rm -rf test_data_js/19/result.txt
-./final 19
-diff test_data_js/19/result.txt test_data_js/19/answer.txt
-
-rm -rf test_data_js/20/result.txt
-./final 20
-diff test_data_js/20/result.txt test_data_js/20/answer.txt
-
-rm -rf test_data_js/21/result.txt
-./final 21
-diff test_data_js/21/result.txt test_data_js/21/answer.txt
-
-rm -rf test_data_js/22/result.txt
-./final 22
-diff test_data_js/22/result.txt test_data_js/22/answer.txt
-
-rm -rf test_data_js/23/result.txt
-./final 23
-diff test_data_js/23/result.txt test_data_js/23/answer.txt
-
-rm -rf test_data_js/24/result.txt
-./final 24
-diff test_data_js/24/result.txt test_data_js/24/answer.txt
+for i in {0..24}
+do
+    rm -rf test_data_js/$i/result.txt
+    echo -
+    echo -
+    echo -------------   test_data: $i  --------------
+    echo -
+    ./final $i
+    diff test_data_js/$i/result.txt test_data_js/$i/answer.txt
+done
